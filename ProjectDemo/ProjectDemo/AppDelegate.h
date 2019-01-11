8877619c3a1e8417e761fe728a0b4ba4ed68551f
yyk <yyk@yykdemacbook-pro.local>
2019-01-11 15:46:42 +0800

    init project

---
+++ ProjectDemo/ProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectDemo
+//
+//  Created by yyk on 2019/1/11.
+//  Copyright © 2019 YYK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectDemo/ProjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectDemo
+//
+//  Created by yyk on 2019/1/11.
+//  Copyright © 2019 YYK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
